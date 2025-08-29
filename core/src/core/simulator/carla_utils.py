f1741350f5820594465f3cc48bc91cd5847fca72
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-08-29 20:30:04 +0800

    Add a short logging str utility for carla.VehicleControl

--- core/src/core/simulator/carla_utils.py
+++ core/src/core/simulator/carla_utils.py
@@ -14,3 +14,14 @@ class CarlaUtils(object):
                 f"xyz:{tf.location.x:.2f}/{tf.location.y:.2f}/{tf.location.z:.2f}; "
                 f"ypr:{tf.rotation.yaw:.2f}/{tf.rotation.pitch:.2f}/{tf.rotation.roll:.2f}"
                 f")")
+
+    @staticmethod
+    def short_direct_control(control: carla.VehicleControl) -> str:
+        """
+        :param control: ``carla.VehicleControl`` 对象
+        :return: 返回一个 ``carla.VehicleControl`` 的短字符串, 用于输出日志
+        """
+        return (f"VehicleControl("
+                f"throttle:{control.throttle:.2f}; "
+                f"steer:{control.steer:.2f}; "
+                f"brake:{control.brake:.2f}; ")
