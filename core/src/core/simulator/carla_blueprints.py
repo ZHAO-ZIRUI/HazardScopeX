a316a37d8bdfd15ad3a8074166a6c09c73b8bbd7
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-08-29 15:30:52 +0800

    Add known bp: sensor.other.collision

--- core/src/core/simulator/carla_blueprints.py
+++ core/src/core/simulator/carla_blueprints.py
@@ -8,3 +8,4 @@ class CarlaBlueprints(Enum):
 
     SENSOR_CAMERA_RGB = "sensor.camera.rgb"
     SENSOR_LIDAR_RAY_CAST = "sensor.lidar.ray_cast"
+    SENSOR_OTHER_COLLISION = "sensor.other.collision"
