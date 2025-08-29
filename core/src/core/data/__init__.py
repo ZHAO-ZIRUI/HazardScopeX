43a54f83af801a70c9dff08f4dd4aa29db769ab2
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-08-29 16:19:54 +0800

    Add incoming collision data

--- core/src/core/data/__init__.py
+++ core/src/core/data/__init__.py
@@ -1,3 +1,4 @@
 from .incoming_data import IncomingData
 from .image import Image
 from .point_cloud import PointCloud
+from .collision import Collision
---
+++ core/src/core/data/collision.py
@@ -0,0 +1,43 @@
+import carla
+from typing_extensions import Self
+
+from core.data import IncomingData
+
+
+class Collision(IncomingData):
+    """
+    碰撞传感器的数据
+    """
+    def __init__(
+            self,
+            frame_id: int,
+            timestamp_sim: float,
+            other_actor_id: int,
+            other_actor_bp_name: str,
+    ) -> None:
+        super().__init__(frame_id, timestamp_sim)
+        self._data = (other_actor_id, other_actor_bp_name)
+
+    @property
+    def other_id(self) -> int:
+        """碰撞"""
+        return self._data[0]
+
+    @property
+    def other_bp_name(self) -> str:
+        return self._data[1]
+
+    @property
+    def is_collision_with_vehicle(self) -> bool:
+        return self.other_bp_name.startswith("vehicle.")
+
+    @classmethod
+    def from_carla(cls, data: carla.CollisionEvent) -> Self:
+        target: carla.Actor = data.other_actor
+        instance = cls(
+            frame_id=data.frame,
+            timestamp_sim=data.timestamp,
+            other_actor_id=target.id,
+            other_actor_bp_name=target.type_id
+        )
+        return instance
