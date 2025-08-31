405aa6fb91a518f6f9a389f0cff3f4a150d57d40
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-08-31 17:39:00 +0800

    Add a quick-return method in CarlaBlueprint utility class for event-type sensors

--- core/src/core/simulator/carla_blueprints.py
+++ core/src/core/simulator/carla_blueprints.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import List
 
 
 class CarlaBlueprints(Enum):
@@ -9,3 +10,9 @@ class CarlaBlueprints(Enum):
     SENSOR_CAMERA_RGB = "sensor.camera.rgb"
     SENSOR_LIDAR_RAY_CAST = "sensor.lidar.ray_cast"
     SENSOR_OTHER_COLLISION = "sensor.other.collision"
+
+    @property
+    def event_sensor_blueprints(self) -> List[str]:
+        return [
+            CarlaBlueprints.SENSOR_OTHER_COLLISION.value,
+        ]
