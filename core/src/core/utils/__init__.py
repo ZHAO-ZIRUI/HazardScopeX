f1f5e26bf3be3aac97778dce558b4ba0b4cd623f
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-09-16 22:36:58 +0800

    Add SharedMemoryUtils to manage different release behaviors for producers and consumers

--- core/src/core/utils/__init__.py
+++ core/src/core/utils/__init__.py
@@ -1,9 +1,11 @@
 from .unique_tag_provider import UniqueTagProvider
 from .route_config import RouteConfig
 from .logging_utils import LogUtils
+from .shm_utils import SharedMemoryUtils
 
 __all__ = [
     'UniqueTagProvider',
     'RouteConfig',
     'LogUtils',
+    'SharedMemoryUtils',
 ]
---
+++ core/src/core/utils/shm_utils.py
@@ -0,0 +1,20 @@
+from multiprocessing.shared_memory import SharedMemory
+from multiprocessing import resource_tracker
+
+
+class SharedMemoryUtils:
+    """
+    SharedMemory 的操作工具, 主要用于处理在 Linux 环境下生产者和消费者之间的不同行为
+    """
+    
+    @staticmethod
+    def producer_close(*shms: SharedMemory):
+        for shm in shms:
+            resource_tracker.unregister(shm.name, 'shared_memory')  # ONLY for Linux
+            shm.close()
+
+    @staticmethod
+    def consumer_close(*shms: SharedMemory):
+        for shm in shms:
+            shm.close()
+            shm.unlink()
