b353e76d0597c830e9de52e69c2c196e234e0632
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-09-17 13:57:27 +0800

    Add operation in RouteConfig to specify return value type

--- core/src/core/utils/route_config.py
+++ core/src/core/utils/route_config.py
@@ -1,19 +1,21 @@
-from typing import Dict, TypeVar
-
-T = TypeVar('T')
+from typing import Dict
 
 
 class RouteConfig(object):
 
+    ALLOWED_TYPES = (bool, float, int, str)
+
     def __init__(self, config: Dict):
         self._config = config
 
-    def get(self, route: str, default: T = None) -> T:
+    def get(self, route: str, default=None, target_type: type = None):
         """
         以路由递归的方式找到深度字典中的值, 路由以 / 分隔
         :param route: 字典路由
+        :param target_type: 目标类型 (bool, float, int, str)
         :param default: 默认返回值
-        :return:
+        :return: 转换后的值
+        :exception ValueError: 当 target_type 不在 ALLOWED_TYPES 中, 或者转换失败时抛出
         """
         if not route:
             return default
@@ -32,6 +34,30 @@ class RouteConfig(object):
                 current = current[key]
             else:
                 return default
-                
-        return current
-        
+        if current is None:
+            return default
+            
+        # 验证目标类型是否合法
+        if target_type is None:
+            target_type = type(default)
+        elif target_type not in self.ALLOWED_TYPES:
+            raise ValueError(f"target_type must be one of {self.ALLOWED_TYPES}, given {target_type}")
+        return self._type_convert(current, target_type)
+
+    @staticmethod
+    def _type_convert(value, target_type: type):
+        try:
+            if target_type == bool:
+                if isinstance(value, str):
+                    return value.lower() in ('true', '1')
+                return bool(value)
+            elif target_type == float:
+                return float(value)
+            elif target_type == int:
+                return int(float(value))
+            elif target_type == str:
+                return str(value)
+            else:
+                return value
+        except (ValueError, TypeError):
+            raise ValueError(f"Cannot convert value '{value}' to {target_type}")
