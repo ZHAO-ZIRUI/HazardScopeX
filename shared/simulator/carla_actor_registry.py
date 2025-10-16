2e6ab08bdb4f485a48da315cfcbea50c86fb99bc
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-10-16 14:46:19 +0800

    Add spawn and destroy methods

--- shared/simulator/carla_actor_registry.py
+++ shared/simulator/carla_actor_registry.py
@@ -1,4 +1,6 @@
-from typing import Dict
+import carla
+from typing import Dict 
+from typing_extensions import Self
 
 from shared.simulator import CarlaActor
 from shared.utils import Logging
@@ -9,7 +11,8 @@ class CarlaActorRegistry:
     CARLA Actor 注册表, 用于管理 CARLA Actor 的生命周期
     """
 
-    def __init__(self):
+    def __init__(self, world: carla.World):
+        self._world = world
         self._actors: Dict[str, CarlaActor] = {}
         self.logger = Logging().get_logger('ActorRegistry')
 
@@ -23,6 +26,16 @@ class CarlaActorRegistry:
     def __len__(self) -> int:
         return len(self._actors)
 
+    @property
+    def world(self) -> carla.World:
+        return self._world
+
+    @world.setter
+    def world(self, value: carla.World):
+        self.logger.warning(f"World is already set. Overwriting with {value.name}")
+        self._world = value
+        return
+
     def add(self, actor: CarlaActor):
         self._actors[actor.id_local] = actor
         self.logger.info(f"Registered actor container '{actor.id_local}'")
@@ -34,3 +47,13 @@ class CarlaActorRegistry:
         del self._actors[actor.id_local]
         self.logger.info(f"Removed actor container '{actor.id_local}'")
         return
+
+    def spawn_all(self) -> Self:
+        for actor in self._actors.values():
+            actor.spawn(self._world)
+        return self
+
+    def destroy_all(self) -> Self:
+        for actor in self._actors.values():
+            actor.destroy(self._world)
+        return self
