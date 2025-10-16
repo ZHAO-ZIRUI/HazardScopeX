6321b5ceac8aa6cbeea5f6ee34f0a3b3e3e9b6bb
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-10-16 14:28:30 +0800

    Update package

--- shared/simulator/__init__.py
+++ shared/simulator/__init__.py
@@ -1,15 +1,19 @@
-from .carla_context import CarlaContext
-from .carla_factory import CarlaFactory
+from .carla_transform import CarlaTransform
+from .carla_blueprints import CarlaBlueprints
 from .carla_actor import CarlaActor
 from .carla_vehicle import CarlaVehicle
 from .carla_sensor import CarlaSensor
-from .carla_blueprints import CarlaBlueprints
+from .carla_actor_registry import CarlaActorRegistry
+from .carla_actor_factory import CarlaActorFactory
+from .carla_context import CarlaContext
 
 __all__ = [
     "CarlaContext",
-    "CarlaFactory",
+    "CarlaActorFactory",
     "CarlaActor",
     "CarlaVehicle",
     "CarlaSensor",
     "CarlaBlueprints",
+    "CarlaTransform",
+    "CarlaActorRegistry",
 ]
