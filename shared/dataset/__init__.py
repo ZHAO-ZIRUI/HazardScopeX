69853512c0a6091ee4ac0c89aca3b4c1d9badc93
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-11-15 01:10:43 +0800

    Add nuscenes dataset dumper

--- shared/dataset/__init__.py
+++ shared/dataset/__init__.py
@@ -1,8 +1,10 @@
 from .dataset_dumper import DatasetDumper
 from .semantic_kitti_dumper import SemanticKittiDumper
+from .nuscenes_dumper import NuScenesDumper
 
 
 __all__ = [
     'DatasetDumper',
     'SemanticKittiDumper',
+    'NuScenesDumper',
 ]  
