c667782cd7a60327c11da0f3bb21ec33541e43d3
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-11-17 17:18:06 +0800

    Add some weather factors

--- apps/ritas_mix1000/factors/__init__.py
+++ apps/ritas_mix1000/factors/__init__.py
@@ -1,9 +1,21 @@
 from .factor_env_noon import FactorEnvNoon
 from .factor_env_sunset import FactorEnvSunset
 from .factor_env_night import FactorEnvNight
+from .factor_weather_heavy_rain import FactorWeatherHeavyRain
+from .factor_weather_cloudiness import FactorWeatherCloudiness
+from .factor_road_flodding import FactorRoadFlodding
+from .factor_weather_dust_storm import FactorWeatherDustStorm
+from .factor_weather_fog import FactorWeatherFog
+from .factor_weather_heavy_fog import FactorWeatherHeavyFog
 
 __all__ = [
     "FactorEnvNoon",
     "FactorEnvSunset",
     "FactorEnvNight",
+    "FactorWeatherHeavyRain",
+    "FactorWeatherCloudiness",
+    "FactorRoadFlodding",
+    "FactorWeatherDustStorm",
+    "FactorWeatherFog",
+    "FactorWeatherHeavyFog",
 ]
