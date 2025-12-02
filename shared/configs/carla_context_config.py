572153b534e92f36bce680ea2c2b7bf18107b3f5
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-12-02 19:10:08 +0800

    Update configs

--- shared/configs/carla_context_config.py
+++ shared/configs/carla_context_config.py
@@ -12,16 +12,17 @@ class CarlaContextConfig(AbstractConfig):
     server_host: str = field(default='127.0.0.1', metadata={'route': 'context/server/host'})
     server_port: int = field(default=2000, metadata={'route': 'context/server/port'})
 
-    server_self_managed_enabled: bool = field(default=False, metadata={'route': 'context/server/use_external_server'})
-    server_self_managed_exe_path: str = field(default='', metadata={'route': 'context/server/exe_path'})
+    server_self_managed_enabled: bool = field(default=False, metadata={'route': 'context/server/self_managed/enabled'})
+    server_self_managed_exe_path: str = field(default='', metadata={'route': 'context/server/self_managed/exe_path'})
 
-    server_multi_gpu_enabled: bool = field(default=False, metadata={'route': 'context/server/use_multi_gpu'})
-    server_multi_gpu_ids: list[int] = field(default_factory=lambda: [0], metadata={'route': 'context/server/gpus'})
-    server_multi_gpu_port_offset: int = field(default=2, metadata={'route': 'context/server/multi_gpu_port_offset'})
-    
-    server_bringup_init_wait_seconds: float = field(default=5.0, metadata={'route': 'context/server/server_start_wait_time'})
-    server_bringup_timeout_seconds: float = field(default=10.0, metadata={'route': 'context/server/server_start_timeout'})
+    server_multi_gpu_enabled: bool = field(default=False, metadata={'route': 'context/server/multi_gpu/enabled'})
+    server_multi_gpu_ids: list[int] = field(default_factory=lambda: [0], metadata={'route': 'context/server/multi_gpu/gpu_ids'})
+    server_multi_gpu_port_offset: int = field(default=2, metadata={'route': 'context/server/multi_gpu/port_offset'})
+
+    server_bringup_init_wait_seconds: float = field(default=5.0, metadata={'route': 'context/server/self_managed/bringup_init_wait_seconds'})
+    server_bringup_timeout_seconds: float = field(default=10.0, metadata={'route': 'context/server/self_managed/bringup_timeout_seconds'})
+    server_bringup_after_kill_wait_seconds: float = field(default=2.0, metadata={'route': 'context/server/self_managed/bringup_after_kill_wait_seconds'})
 
     # 运行时配置
-    runtime_sync_mode_fps: float = field(default=20.0, metadata={'route': 'context/server/sync_mode_fps'})
-    runtime_timeout_seconds: float = field(default=10.0, metadata={'route': 'context/server/timeout'})
+    runtime_sync_mode_fps: float = field(default=20.0, metadata={'route': 'context/server/runtime/sync_mode_fps'})
+    runtime_timeout_seconds: float = field(default=10.0, metadata={'route': 'context/server/runtime/timeout_seconds'})
