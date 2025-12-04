19d826f8f14f35e22d94d97524b60313cbf44b32
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-12-04 16:45:23 +0800

    Fix import sequence

--- shared/simulator/__init__.py
+++ shared/simulator/__init__.py
@@ -1,5 +1,7 @@
-from .carla_transform import CarlaTransform
+from .carla_maps import CarlaMaps
 from .carla_blueprints import CarlaBlueprints
+from .carla_tick_blocker import CarlaTickBlocker
+from .carla_transform import CarlaTransform
 from .carla_actor import CarlaActor
 from .carla_vehicle import CarlaVehicle
 from .carla_sensor import CarlaSensor
@@ -7,8 +9,6 @@ from .carla_actor_manager import CarlaActorManager
 from .carla_io_manager import CarlaIOManager
 from .carla_recorder import CarlaRecorder
 from .carla_context import CarlaContext
-from .carla_tick_blocker import CarlaTickBlocker
-from .carla_maps import CarlaMaps
 
 __all__ = [
     "CarlaMaps",
