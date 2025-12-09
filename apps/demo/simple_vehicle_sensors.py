25317b062bc0d279a8e8ca974467be06f5cb1b28
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-12-09 12:40:50 +0800

    Update demo scripts

--- apps/demo/simple_vehicle_sensors.py
+++ apps/demo/simple_vehicle_sensors.py
@@ -1,22 +1,35 @@
-# 最简车辆与传感器程序, 用于测试通信功能
+# ==============================================================
+# 简单的车辆与传感器程序样例
+# 
 # 在 CARLA 中创建一辆车辆, 并为其安装传感器, 然后开始 CARLA AUTOPILOT 自动驾驶
-from pathlib import Path
-from shared.simulator import *
+# 用于测试通信功能
+#
+#
+# 逻辑：
+# 1. 创建一辆车辆
+# 2. 为车辆安装多个传感器（前置相机、游戏视角相机、激光雷达）
+# 3. 绑定传感器输出到 ROS2 和共享内存
+# 4. 启动 CARLA AUTOPILOT 自动驾驶
+# ==============================================================
+from shared.simulator import CarlaContext, CarlaBlueprints, CarlaTransform
 from shared.utils import Logging
+from shared.prefabs import PlayerVehicle
 
 if __name__ == "__main__":
-    # 基础组件初始化
-    config = Path('config.yaml')                            # 读取配置文件
-    logger = Logging.load(config).get_logger('Main')        # 设置日志记录器
+    logger = Logging.load('config.yaml').get_logger('Main')
+    logger.info('DEMO FOR VEHICLE AND SENSORS')
 
-    logger.info('Starting simple server')
-    with CarlaContext(config) as context:
 
+    with CarlaContext() as context:
+
+# region: 以手工方式创建车辆与传感器
+        # 创建一辆车辆
         vehicle = context.actors.create_vehicle(
             bp=CarlaBlueprints.VEHICLE_AUDI_A2,
             tf=context.spawn_points[0],
         )
 
+        # 创建前置相机传感器
         cam_front = context.actors.create_sensor(
             bp=CarlaBlueprints.SENSOR_CAMERA_RGB,
             name='CAM_FRONT',
@@ -24,6 +37,7 @@ if __name__ == "__main__":
             parent=vehicle,
         )
 
+        # 创建游戏视角相机传感器
         cam_game = context.actors.create_sensor(
             bp=CarlaBlueprints.SENSOR_CAMERA_RGB,
             name='CAM_GAME',
@@ -31,30 +45,40 @@ if __name__ == "__main__":
             parent=vehicle,
         )
 
+        # 创建激光雷达传感器
         lidar_main = context.actors.create_sensor(
             bp=CarlaBlueprints.SENSOR_LIDAR_RAY_CAST,
-            name='LIDAR_MAIN',
+            name='LIDAR',
             tf=CarlaTransform(x=0.0, y=0.0, z=2.2),
             parent=vehicle,
         )
 
+# endregion
+
+# region: 以预制车辆方式创建车辆与传感器
+        # 与上述手工方式等价, 但更简洁
+        # vehicle = PlayerVehicle(context, context.spawn_points[0])
+        # cam_front = vehicle.cam_front
+        # cam_game = vehicle.cam_game
+        # lidar_main = vehicle.lidar
+# endregion
+
+        # 等待车辆稳定
         context.actors.spawn_all()
         context.actors.wait_stable(vehicle)
 
+        # 绑定传感器输出到共享内存
         context.io.create_shm(topic='sensor_data').bind_sensor_output(cam_front)
 
-        # NOTE: create_ros2 在传感器回调中中发布消息, 会产生一定的性能问题
-        # context.io.create_ros2(topic='/harzed_scope/cam/front').bind_sensor_output(cam_front)
-        # context.io.create_ros2(topic='/harzed_scope/cam/game').bind_sensor_output(cam_game)
-        # context.io.create_ros2(topic='/harzed_scope/lidar/main').bind_sensor_output(lidar_main)
+        # 绑定传感器输出到 ROS2
+        context.io.create_ros2(topic='/harzed_scope/cam/front').bind_sensor_output(cam_front)
+        context.io.create_ros2(topic='/harzed_scope/cam/game').bind_sensor_output(cam_game)
+        context.io.create_ros2(topic='/harzed_scope/lidar/main').bind_sensor_output(lidar_main)
 
-        # NOTE: create_ros2_hp 先使用 SHM, 再在单独的子进程中处理 ROS2 的消息发布, 可以避免因为 Python GIL 锁导致的问题
-        context.io.create_ros2_hp(ros_topic_name='/harzed_scope/cam/front').bind_sensor_output(cam_front)
-        context.io.create_ros2_hp(ros_topic_name='/harzed_scope/cam/game').bind_sensor_output(cam_game)
-        context.io.create_ros2_hp(ros_topic_name='/harzed_scope/lidar/main').bind_sensor_output(lidar_main)
-
+        # 启动 CARLA AUTOPILOT 自动驾驶
         vehicle.set_carla_autopilot(enable=True)
 
         # 启动主线程阻塞循环
         context.spin()
-    logger.info('Goodbye!')
+
+    logger.info('GOODBYE!')
