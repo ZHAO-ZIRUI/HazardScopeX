2f4c3b271f15c12b5f4f43d76626f51e97ad6194
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-12-09 21:37:19 +0800

    Refactor facotr and injector lifecycle

--- shared/scenarios/factor.py
+++ shared/scenarios/factor.py
@@ -1,36 +1,149 @@
-from abc import ABC
-from typing import TYPE_CHECKING
+from abc import ABC, abstractmethod
+from logging import Logger
+from enum import Enum, auto
 
 from shared.utils import Logging
-
-if TYPE_CHECKING:
-    from shared.simulator import CarlaContext
+from shared.simulator import CarlaContext, CarlaActor
 
 
 class Factor(ABC):
     """
     场景因子, 用于在仿真中注入特定的场景因子
+
+    因子定义包含以下内容:
+    - strength: 归一化表示的因子强度, 在 __init__ 阶段中赋值, 范围为 0.0 到 1.0, 默认为 1.0
+    - PRIORITY: 因子优先级, 用于在多个因子联合注入时, 控制因子的执行顺序, 范围为整型区间, 越小越先执行
+    - result:   归一化表示的因子执行评估结果, 范围为 0.0 到 1.0, 在因子未处于 FINISHED 状态时为 -1.0
+
+    因子的生命周期如下:
+    - HANGUP         # 挂起, 因子尚未开始执行
+    - bringup()      # 初始化, 用于生成场景实例
+    - warmup() x N   # 预热, 用于启动车辆或车流等, 会执行多帧, 直到因子处于 UPDATE 状态
+    - update() x N   # 帧更新, 用于更新实例状态或进行触发, 会执行多帧
+    - teardown() x N # 销毁, 用于销毁场景实例, 会执行多帧, 直到因子处于 FINISHED 状态
+    - FINISHED       # 完成, 用于标记因子生命周期结束
+
+    因子的命名请修改类属性 NAME, 遵循以下命名规则:
+    - 以 Factor 开始, 使用 PascalCase(大驼峰) 命名
+    - 第二个单词表示因子的类型
+        - Env: 环境因素, 包括天气, 时间, 光照等
+        - Traffic: 车流因素, 与主要事件关系不大的车流因素
+        - Sensor: 传感器因素, 包括对相机, 激光雷达, 毫米波雷达等传感器的数据处理
+        - Case: 交互因素, 具体的交互场景
+    - 第三个至以后的单词表示因子的具体说明
+    - 示例:
+        - FactorEnvHeavyRain
+        - FactorTrafficLargeVehilces
+        - FactorSensorCamLoss
+        - FactorCaseForceCutin
     """
+
     NAME = 'Factor'
-    PRIORITY = False    # 当设置地图时, 设置PRIORITY为True
+    PRIORITY = 0
 
-    def __init__(self, context: 'CarlaContext'):
-        self.logger = Logging().get_logger(self.NAME)
+    class FactorStatus(Enum):
+        HANGUP = auto()
+        BRINGUP = auto()
+        WARMUP = auto()
+        UPDATE = auto()
+        TEARDOWN = auto()
+        FINISHED = auto()
+
+    def __init__(self, context: CarlaContext, strength: float = 1.0):
+        self._logger = Logging().get_logger(self.NAME)
         self._context = context
+        self._status = self.FactorStatus.HANGUP
+        self._strength = strength
+        self._result: float = -1.0
+        self._actors: dict[str, CarlaActor] = {}
+        
+        self._is_warmup_completed = False
+        self._is_teardown_completed = False
+        self._is_update_ended = False
 
-    def setup(self) -> None:
-        """因子要件的初始化逻辑
-        """
-        self.logger.info(f'Setup completed')
+    @property
+    def logger(self) -> Logger:
+        return self._logger
+
+    @property
+    def status(self) -> FactorStatus:
+        return self._status
+
+    @property
+    def is_update_ended(self) -> bool:
+        """因子更新是否结束, 用于标记因子更新周期的结束, 只读"""
+        return self._is_update_ended
+
+    @property
+    def is_warmup_completed(self) -> bool:
+        """因子预热是否完成, 只读"""
+        return self._is_warmup_completed
+
+    @property
+    def is_teardown_completed(self) -> bool:
+        """因子销毁是否完成, 只读"""
+        return self._is_teardown_completed
+
+    @property
+    def strength(self) -> float:
+        """因子强度, 用于控制因子的强度, 范围为 0.0 到 1.0, 默认为 1.0, 只读"""
+        return self._strength
+
+    @status.setter
+    def status(self, value: FactorStatus):
+        before = self._status
+        self._status = value
+        self.logger.debug(f'Status changed: {before.name} -> {value.name}')
         return self
 
-    def tick(self) -> None:
-        """在每个 tick 中执行因子逻辑
+    @property
+    def result(self) -> float:
+        """因子执行评估结果, 范围为 0.0 到 1.0, 在因子未处于 FINISHED 状态时, 结果为 -1.0, 只读"""
+        return self._result
+
+    def bringup(self) -> None:
+        """因子要件的初始化逻辑, 用于生成场景实例, 只会执行一次"""
+        self.status = self.FactorStatus.BRINGUP
+        return self
+
+    @abstractmethod
+    def warmup(self) -> None:
+        """因子要件的预热逻辑, 用于启动车辆或车流等, 会执行多帧
+        
+        该方法必须被子类实现, 并手动设置 self._is_warmup_completed 为 True 以标记预热完成
         """
-        return
+        raise NotImplementedError
+        if self._is_warmup_completed:
+            return
+        self.status = self.FactorStatus.WARMUP
+        return self
+
+    @abstractmethod
+    def update(self) -> None:
+        """因子要件的更新逻辑, 用于更新实例状态或进行触发, 会执行多帧
+        
+        该方法必须被子类实现, 并手动设置 self._is_update_ended 为 True 以标记更新结束
+        """
+        if self._is_update_ended:
+            return
+        self.status = self.FactorStatus.UPDATE
+        self._result = self._evaluate()
+        return self
 
     def teardown(self) -> None:
-        """因子要件的销毁逻辑
-        """
-        self.logger.info(f'Teardown completed')
+        """因子要件的销毁逻辑, 用于销毁场景实例, 只会执行一次"""
+
+        self.status = self.FactorStatus.TEARDOWN
+
+        # 销毁所有场景 Actor 实例
+        for actor in self._actors.values():
+            actor.destroy()
+        self._actors.clear()
+
+        self._is_teardown_completed = True
+        self.status = self.FactorStatus.FINISHED
         return self
+
+    def _evaluate(self) -> float:
+        """因子执行评估逻辑, 用于评估因子的执行结果"""
+        return self._result
--- shared/scenarios/injector.py
+++ shared/scenarios/injector.py
@@ -1,32 +1,37 @@
-import threading
-from typing import TYPE_CHECKING, List
+import time
 from typing_extensions import Self
+from logging import Logger
 
-from shared.utils import Logging
+from shared.utils import Logging, PostInitMeta
 from shared.scenarios import Factor
-
-if TYPE_CHECKING:
-    from shared.simulator import CarlaContext
+from shared.simulator import CarlaContext, CarlaTickBlocker
 
 
-class Injector:
+class Injector(metaclass=PostInitMeta):
     """
     注入器, 用于在仿真中注入特定的场景因子
     """
 
-    TICK_BLOCKER_TOKEN = 'injector'
+    TICK_BLOCKER_TOKEN = 'Injector'
 
-    def __init__(self, context: 'CarlaContext', *factors: Factor):
-        self.logger = Logging().get_logger('Injector')
+    def __init__(self, context: CarlaContext, flag_auto_tick: bool = True, *factors: Factor | None):
+        self._logger = Logging().get_logger('Injector')
         self._context = context
-        self._factors: List[Factor] = list(factors)
+        self._factors: list[Factor | None] = list(factors)
+        self._flag_auto_tick = flag_auto_tick
 
-        self._tick_blocker = threading.Event()
+        self._status = Factor.FactorStatus.HANGUP
 
-        self._post_init()
+        self._tick_blocker = CarlaTickBlocker(name=self.TICK_BLOCKER_TOKEN)
 
-    def _post_init(self) -> Self:
-        self._context.bind_tick_blocker(self.TICK_BLOCKER_TOKEN, self._tick_blocker)
+    def __post_init__(self) -> Self:
+        # 清理 factors 中为 None 的因子
+        self._factors = [factor for factor in self._factors if factor is not None]
+
+        # 绑定 TickBlocker
+        self._context.tick_blockers.append(self._tick_blocker)
+
+        # 打印日志
         self.logger.info(f'Initialized with {len(self._factors)} factors')
         self.logger.debug(f'Factors: {", ".join([factor.NAME for factor in self._factors])}')
         return self
@@ -39,27 +44,88 @@ class Injector:
         self.teardown()
         return
 
-    def setup(self) -> None:
-        for factor in sorted(self._factors, key=lambda x: x.PRIORITY, reverse=True):
-            if factor.PRIORITY:
-                self._context.flag_ignore_dead_detector = True
-            factor.setup()
-            self._context.tick()
-            self._context.flag_ignore_dead_detector = False
+    @property
+    def logger(self) -> Logger:
+        return self._logger
 
-        self._context.hook_before_tick.append(self.tick)
+    def setup(self) -> None:
+        for factor in sorted(self._factors, key=lambda x: x.PRIORITY):
+            factor.bringup()
+            self._context.tick()
+
+        # 绑定 TICK 钩子
+        if self._flag_auto_tick:
+            self._context.hook_on_tick.append(self.tick)
+
+        self._status = Factor.FactorStatus.BRINGUP
         self.logger.info(f'All factors setup completed')
 
     def tick(self) -> None:
         self._tick_blocker.set()
+
+        if self._status == Factor.FactorStatus.BRINGUP:
+            self._status = Factor.FactorStatus.WARMUP
         
-        for factor in self._factors:
-            factor.tick()
+        # WARMUP 阶段
+        if self._status == Factor.FactorStatus.WARMUP:
+            if all(factor.is_warmup_completed for factor in self._factors):
+                self._status = Factor.FactorStatus.UPDATE
+                self.logger.info(f'All factors warmup completed, begin to update ...')
+                self._tick_blocker.clear()
+                return
+            else:
+                for factor in self._factors:
+                    factor.warmup()
+        
+        # UPDATE 阶段
+        if self._status == Factor.FactorStatus.UPDATE:
+            if any(factor.is_update_ended for factor in self._factors):
+                self._status = Factor.FactorStatus.TEARDOWN
+                updated_ended_factor_list = [factor for factor in self._factors if factor.is_update_ended]
+                self.logger.info(f'Factors update ended: {", ".join([factor.NAME for factor in updated_ended_factor_list])}')
+                self.logger.info(f'Found factors update ended, begin to teardown ...')
+                self._tick_blocker.clear()
+                return
+            else:
+                for factor in self._factors:
+                    factor.update()
+        
+        # TEARDOWN 阶段
+        if self._status == Factor.FactorStatus.TEARDOWN:
+            if all(factor.is_teardown_completed for factor in self._factors):
+                self.logger.info(f'All factors teardown completed')
+                self._status = Factor.FactorStatus.FINISHED
+                self._tick_blocker.clear()
+                return
+            else:
+                for factor in self._factors:
+                    factor.teardown()
         
         self._tick_blocker.clear()
 
     def teardown(self) -> None:
-        for factor in self._factors:
-            factor.teardown()
-        self.logger.info(f'All factors teardown completed')
+        # 如果调用该方法时, 因子尚未完成, 则跳过当前步骤直接执行 TEARDOWN 阶段
+        while self._status != Factor.FactorStatus.FINISHED:
+            # 如果处于挂起阶段, 则直接设置为 FINISHED 状态
+            if self._status == Factor.FactorStatus.HANGUP:
+                self._status = Factor.FactorStatus.FINISHED
+                break
+            # 否则, 设置为 TEARDOWN 阶段, 并执行 TICK
+            self._status = Factor.FactorStatus.TEARDOWN
+            self.tick()
+
+        # 移除 TICK 钩子
+        if self._flag_auto_tick:
+            self._context.hook_on_tick.remove(self.tick)
+
         return self
+
+    def spin_until_finished(self) -> None:
+        while self._status != Factor.FactorStatus.FINISHED:
+            self._context.tick()
+            try:
+                time.sleep(self._context.calc_tick_wait_time())
+            except KeyboardInterrupt:
+                self.logger.warning('Spin interrupted by user')
+                raise SystemExit(401)
+        return
