c73a9c623c33432fe34ab09588a4c15c69e268aa
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-12-09 15:38:33 +0800

    Add timeit utility for performance measurement

--- shared/utils/__init__.py
+++ shared/utils/__init__.py
@@ -1,9 +1,11 @@
 from .logging import Logging
 from .id_generator import IdGenerator
 from .post_init_meta import PostInitMeta
+from .timeit import timeit
 
 __all__ = [
     "Logging",
     "IdGenerator",
     "PostInitMeta",
+    "timeit",
 ]
