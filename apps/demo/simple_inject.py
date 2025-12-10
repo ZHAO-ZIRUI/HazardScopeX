cfa8b5bdca268501a63b0f245feeb8a8f752e0ae
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-12-10 15:02:30 +0800

    Update example for inject

--- apps/demo/simple_inject.py
+++ apps/demo/simple_inject.py
@@ -1,8 +1,13 @@
-# 简单的因子注入样例
-import numpy as np
+# ==============================================================
+# 简单的因子注入程序样例
+# 
+# 以继承方式实现了两个因子, 并执行注入过程
+#
+# 因子 FactorSensorCameraTag: 传感器注入样例, 在相机画面上中心绘制一个 DEMO FACTOR SENSOR TAG 标签
+# 因子 FactorDemoTrigger: 事件触发样例, 等待 3 秒后触发, 然后等待 3 秒后完成
+# ==============================================================
 import cv2
-import random
-from pathlib import Path
+from typing_extensions import Self
 from shared.data.image import Image
 from shared.simulator import *
 from shared.utils import Logging
@@ -10,20 +15,22 @@ from shared.prefabs import PlayerVehicle
 from shared.scenarios import Injector, Factor
 
 
-class FactorImageTag(Factor):
-    NAME = 'F_ImageTag'
+class FactorSensorCameraTag(Factor):
+    NAME = 'F_SensorCameraTag'
 
-    def __init__(self, context: 'CarlaContext', sensor: CarlaSensor):
+    def __init__(self, context: CarlaContext, camera: CarlaSensor):
         super().__init__(context)
-        self._sensor = sensor
+        assert isinstance(camera, CarlaSensor) and camera.is_camera
+        self._camera = camera        
 
-    def setup(self) -> None:
-        self._sensor.hook_sensor_data_recv.append(self.on_sensor_data_recv)
-        return super().setup()
+    def __post_init__(self) -> Self:
+        self.hook_bringup.append(lambda: self._camera.hook_sensor_data_recv.append(self.on_sensor_data_recv))
+        self.hook_teardown.append(lambda: self._camera.hook_sensor_data_recv.remove(self.on_sensor_data_recv))
+        return self
 
     def on_sensor_data_recv(self, data: Image) -> Image:
         # 设置文字参数
-        text = 'DEMO FACTOR'
+        text = 'DEMO FACTOR SENSOR TAG'
         font = cv2.FONT_HERSHEY_SIMPLEX
         font_scale = 1.0
         thickness = 2
@@ -48,51 +55,69 @@ class FactorImageTag(Factor):
         # 绘制白色文字 (BGRA格式: B=255, G=255, R=255, A=255)
         cv2.putText(data._raw, text, (text_x, text_y), font, font_scale, (255, 255, 255), thickness, cv2.LINE_AA)
         
+        self.stage = self.FactorStage.COMPLETED
+
         return data
 
-class FactorSensorDataRandomLoss(Factor):
-    NAME = 'F_SensorDataRandomLoss'
-    PRIORITY = True
+class FactorDemoTrigger(Factor):
+    NAME = 'F_DemoTrigger'
 
-    def __init__(self, context: 'CarlaContext', sensor: CarlaSensor):
+    def __init__(self, context: CarlaContext):
         super().__init__(context)
-        self._sensor = sensor
-        self._last_data: Image | None = None
+        self._wait_trigger_seconds = 3.0
+        self._triggered_seconds = 3.0
 
-    def setup(self) -> None:
-        self._sensor.hook_sensor_data_recv.append(self.on_sensor_data_recv)
-        return super().setup()
+        self._count_before_trigger = 0
+        self._count_after_trigger = 0
 
-    def on_sensor_data_recv(self, data: Image) -> Image:
-        if random.random() < 0.9:
-            return self._last_data
-        else:
-            self._last_data = data
-            return data
+    def __post_init__(self) -> Self:
+        self.hook_update.append(self.trigger)
+        self.hook_update.append(self.post_trigger)
+        return self
+
+    def trigger(self) -> None:
+        # 如果因子不在等待触发阶段, 则直接返回
+        if self.stage != self.FactorStage.WAIT_FOR_TRIGGER:
+            return
+        # 如果等待触发帧数达到阈值, 则触发因子
+        if self._count_before_trigger >= self._wait_trigger_seconds * self._context.fps:
+            self.stage = self.FactorStage.TRIGGERED
+            self.logger.warning(f'Factor {self.NAME} triggered')  # 以警告级别输出
+        self._count_before_trigger += 1
+        return
+
+    def post_trigger(self) -> None:
+        # 如果因子不在触发阶段, 则直接返回
+        if self.stage != self.FactorStage.TRIGGERED:
+            return
+        # 如果触发帧数达到阈值, 则完成因子
+        if self._count_after_trigger >= self._triggered_seconds * self._context.fps:
+            self.stage = self.FactorStage.COMPLETED
+            self.logger.warning(f'Factor {self.NAME} completed')  # 以警告级别输出
+        self._count_after_trigger += 1
+        return
+
 
 if __name__ == "__main__":
-    # 基础组件初始化
-    config = Path('config.yaml')                            # 读取配置文件
-    logger = Logging.load(config).get_logger('Main')        # 设置日志记录器
+    logger = Logging.load('config.yaml').get_logger('Main')
+    logger.info('DEMO FOR SIMPLE INJECT')
 
-    with CarlaContext(config) as context:
-        context.change_map('Town04')
-
+    with CarlaContext() as context:    
         vehicle = PlayerVehicle(context, context.spawn_points[0])
 
         context.actors.spawn_all()
         context.actors.wait_stable(vehicle)
 
-        context.io.create_ros2_hp(ros_topic_name='/harzed_scope/cam/game').bind_sensor_output(vehicle.cam_game)
-        context.io.create_ros2_hp(ros_topic_name='/harzed_scope/lidar/main').bind_sensor_output(vehicle.lidar_main)
+        context.io.create_ros2(topic='/harzed_scope/cam/game').bind_sensor_output(vehicle.cam_game)
+        context.io.create_ros2(topic='/harzed_scope/lidar/main').bind_sensor_output(vehicle.lidar)
 
         vehicle.set_carla_autopilot(enable=True)
 
-        f1 = FactorImageTag(context, vehicle.cam_game)
-        f2 = FactorSensorDataRandomLoss(context, vehicle.cam_game)
+        f1 = FactorSensorCameraTag(context, vehicle.cam_game)
+        f2 = FactorDemoTrigger(context)
         factors = [f1, f2]
         
         with Injector(context, *factors) as injector:       # 执行注入
-            context.spin()
+            injector.spin_until_finished(f2)
 
     logger.info('Goodbye!')
