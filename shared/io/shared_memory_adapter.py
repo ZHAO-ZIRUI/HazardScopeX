5a439bbd7c942b9a9654f2143c642d6976c6937c
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-12-16 21:36:09 +0800

    Update critical logic: server side always create shared memory as producers

--- shared/io/shared_memory_adapter.py
+++ shared/io/shared_memory_adapter.py
@@ -10,11 +10,10 @@ class SharedMemoryAdapter(AbstractIOAdapter):
     共享内存适配器, 用于将仿真器的数据转换为共享内存数据
     """
 
-    def __init__(self, shm: SharedMemory, topic: str, managed:bool = True):
+    def __init__(self, shm: SharedMemory, topic: str):
         super().__init__()
         self._shared_memory_instance = shm
         self._topic = topic
-        self._managed = managed
 
     @property
     def shared_memory_instance(self) -> SharedMemory:
@@ -25,11 +24,6 @@ class SharedMemoryAdapter(AbstractIOAdapter):
     def topic(self) -> str:
         """共享内存的名称, 与对象创建时的 topic 参数一致"""
         return self._topic
-    
-    @property
-    def managed(self) -> bool:
-        """是否由 IOManager 创建"""
-        return self._managed
 
     def bind_sensor_output(self, sensor: CarlaSensor) -> Self:
         sensor.hook_sensor_data_ready.append(
