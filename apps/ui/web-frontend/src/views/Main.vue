22fd63e07d7257ed8a0faeb280193cefc56f15a5
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-12-22 18:41:28 +0800

    Update layout

--- apps/ui/web-frontend/src/views/Main.vue
+++ apps/ui/web-frontend/src/views/Main.vue
@@ -7,9 +7,9 @@ import MainMenuBottom from '../components/layout/MainMenuBottom.vue';
   <div class="layout-root-container">
     <MainMenuTop />
     <div class="layout-main-container">
-      <div class="layout-main-container sidebar">Left Sidebar</div>
+      <div class="layout-main-container left-sidebar">Left Sidebar</div>
       <div class="layout-main-container content">Main Content</div>
-      <div class="layout-main-container sidebar">Right Sidebar</div>
+      <div class="layout-main-container right-sidebar">Right Sidebar</div>
     </div>
     <MainMenuBottom />
   </div>
@@ -31,7 +31,7 @@ import MainMenuBottom from '../components/layout/MainMenuBottom.vue';
 
 .layout-main-container.left-sidebar {
   flex: 1;
-  background-color: rgb(125, 137, 131);
+  background-color: rgb(141, 90, 134);
 }
 
 .layout-main-container.content {
@@ -41,6 +41,6 @@ import MainMenuBottom from '../components/layout/MainMenuBottom.vue';
 
 .layout-main-container.right-sidebar {
   flex: 1;
-  background-color: rgb(125, 137, 131);
+  background-color: rgb(109, 180, 144);
 }
 </style>
