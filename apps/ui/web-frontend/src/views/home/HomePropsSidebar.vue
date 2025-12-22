d0a190a848694a29bf9822f672fb610deaaad13f
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-12-22 21:34:48 +0800

    Enhance layout and interactivity

--- apps/ui/web-frontend/src/views/home/HomePropsSidebar.vue
+++ apps/ui/web-frontend/src/views/home/HomePropsSidebar.vue
@@ -1,7 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ResizableContainer from '@/components/ResizableContainer.vue';
+import ToggleContainer from '@/components/ToggleContainer.vue';
+</script>
 <template>
-  PROPS SIDEBAR
+  <div class="home-props-sidebar">
+    <ResizableContainer direction="column">
+      <ToggleContainer name="NODE">
+        <div>ITEM 1</div>
+        <div>ITEM 2</div>
+      </ToggleContainer>
+      <ToggleContainer name="PROJECT">
+        <div>ITEM 3</div>
+        <div>ITEM 4</div>
+      </ToggleContainer>
+    </ResizableContainer>
+  </div>
 </template>
 <style scoped>
-
+.home-props-sidebar {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+}
 </style>
