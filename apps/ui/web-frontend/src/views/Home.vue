4abfa3c726a9e9fbbe4330e973a3c73c17db4ff6
Z.R.ZHAO <zhaozr@mail.sustech.edu.cn>
2025-12-22 19:48:12 +0800

    Use ResizableContainer to management layout

--- apps/ui/web-frontend/src/views/Home.vue
+++ apps/ui/web-frontend/src/views/Home.vue
@@ -4,22 +4,17 @@ import HomeMenuBottom from '@/views/home/HomeMenuBottom.vue';
 import HomeJobSidebar from '@/views/home/HomeJobSidebar.vue';
 import HomePropsSidebar from '@/views/home/HomePropsSidebar.vue';
 import HomeFlow from '@/views/home/HomeFlow.vue';
+import ResizableContainer from '@/components/ResizableContainer.vue';
 </script>
   
 <template>
   <div class="layout-root-container">
     <HomeMenuTop />
-    <div class="layout-main-container">
-      <div class="layout-main-container left-sidebar">
-        <HomeJobSidebar />
-      </div>
-      <div class="layout-main-container content">
-        <HomeFlow />
-      </div>
-      <div class="layout-main-container right-sidebar">
-        <HomePropsSidebar />
-      </div>
-    </div>
+    <ResizableContainer class="layout-main-container" direction="row">
+      <HomeJobSidebar weight="1"/>
+      <HomeFlow weight="4"/>
+      <HomePropsSidebar weight="1"/>
+    </ResizableContainer>
     <HomeMenuBottom />
   </div>
 </template>
@@ -29,29 +24,5 @@ import HomeFlow from '@/views/home/HomeFlow.vue';
   display: flex;
   flex-direction: column;
   height: 100vh;
-}
-
-.layout-main-container {
-  flex: 1;
-  overflow: auto;
-  display: flex;
-  flex-direction: row;
-}
-
-.layout-main-container.left-sidebar {
-  flex: 1;
-  border-right: 1px solid var(--color-layout-border);
-  /* background-color: rgb(141, 90, 134); */
-}
-
-.layout-main-container.content {
-  flex: 4;
-  /* background-color: rgb(125, 137, 131); */
-}
-
-.layout-main-container.right-sidebar {
-  flex: 1;
-  border-left: 1px solid var(--color-layout-border);
-  /* background-color: rgb(109, 180, 144); */
 }
 </style>
