855e45785c4e29c173a26906b85a299468da2ffd
ZHAO-ZIRUI <zhaozr@mail.sustech.edu.cn>
2026-01-04 11:17:18 +0800

    Update code comment

--- apps/demo/simple_server_debug.py
+++ apps/demo/simple_server_debug.py
@@ -1,5 +1,5 @@
 # ==============================================================
-# 简单的服务器程序样例
+# 简单的服务器程序样例, 包含 Debug 工具的使用样例
 # 
 # 启动 CARLA 服务器并保持运行
 #
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     with CarlaContext() as context:
         # 启动主线程阻塞循环
-        with context.log_tick_details():
+        with context.log_tick_details(): # 在 Tick 过程中打印详细信息
             context.spin()
 
     logger.info('GOODBYE!')
