37d1e3693c3a4754675cbafc57e3d6e4ace8b355
ZHAO-ZIRUI <zhaozr@ymail.sustech.edu.cn>
2026-01-27 18:36:50 +0800

    Add factor: construction area

--- shared/scenarios/factorlib/__init__.py
+++ shared/scenarios/factorlib/__init__.py
@@ -1,9 +1,11 @@
 from .f_case_vehicle_drop_obstacle import FactorCaseVehicleDropObstacle
 from .f_case_dart_out_obstacle import FactorCaseDartOutObstacle
 from .f_case_static_obstacle import FactorStaticObstacle
+from .f_case_construction_area import FactorCaseConstructionArea
 
 __all__ = [
     "FactorCaseVehicleDropObstacle",
     "FactorCaseDartOutObstacle",
     "FactorStaticObstacle",
+    "FactorCaseConstructionArea",
 ]
