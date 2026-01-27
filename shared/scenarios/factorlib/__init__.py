6c7f2714177f5756a9e52fd9bb398be938d23d94
ZHAO-ZIRUI <zhaozr@ymail.sustech.edu.cn>
2026-01-27 10:30:50 +0800

    Refactor factor name

--- shared/scenarios/factorlib/__init__.py
+++ shared/scenarios/factorlib/__init__.py
@@ -1,5 +1,5 @@
-from .f_case_drop_obstical import FactorCaseDropObstical
+from .f_case_vehicle_drop_obstical import FactorCaseVehicleDropObstical
 
 __all__ = [
-    "FactorCaseDropObstical",
+    "FactorCaseVehicleDropObstical",
 ]
